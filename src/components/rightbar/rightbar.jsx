75bfe685d6c9ee3735fc98aad51e894f52a72059
Jehanzaib <jehanzaib.dev@gmail.com>
2026-03-31 10:37:42 +0500

    rightbar completed layout completed

--- src/components/rightbar/rightbar.jsx
+++ src/components/rightbar/rightbar.jsx
@@ -11,10 +11,60 @@ return(
 				<span className="birthdayReminder"><b>Jacky Short </b>and <b>3 others</b> have birthday today.</span>
 				
 			</div>
-			
+			<img src="assets/ad.png" alt="" className="rightbarAd"/>
+			<h4 className="onlineTitle">Online Friends</h4>
+			<ul className="onlineFriendsList">
+				<li className="onlineFriendsItem">
+				<div className="rightbarOnlineContainer">
+				<img src="assets/person/3.jpeg" className="rightbarOnlineImg"/> 
+				<span className="onlineBadge"></span>
+				</div>
+				<span className="rightbarOnlineFriendName">Jake Carter</span>
+				</li>
+				<li className="onlineFriendsItem">
+				<div className="rightbarOnlineContainer">
+				<img src="assets/person/3.jpeg" className="rightbarOnlineImg"/> 
+				<span className="onlineBadge"></span>
+				</div>
+				<span className="rightbarOnlineFriendName">Jake Carter</span>
+				</li>
+				<li className="onlineFriendsItem">
+				<div className="rightbarOnlineContainer">
+				<img src="assets/person/3.jpeg" className="rightbarOnlineImg"/> 
+				<span className="onlineBadge"></span>
+				</div>
+				<span className="rightbarOnlineFriendName">Jake Carter</span>
+				</li>
+				<li className="onlineFriendsItem">
+				<div className="rightbarOnlineContainer">
+				<img src="assets/person/3.jpeg" className="rightbarOnlineImg"/> 
+				<span className="onlineBadge"></span>
+				</div>
+				<span className="rightbarOnlineFriendName">Jake Carter</span>
+				</li>
+				<li className="onlineFriendsItem">
+				<div className="rightbarOnlineContainer">
+				<img src="assets/person/3.jpeg" className="rightbarOnlineImg"/> 
+				<span className="onlineBadge"></span>
+				</div>
+				<span className="rightbarOnlineFriendName">Jake Carter</span>
+				</li>
+				<li className="onlineFriendsItem">
+				<div className="rightbarOnlineContainer">
+				<img src="assets/person/3.jpeg" className="rightbarOnlineImg"/> 
+				<span className="onlineBadge"></span>
+				</div>
+				<span className="rightbarOnlineFriendName">Jake Carter</span>
+				</li>
+			</ul>
+
 		</div>
 	
 
 	</div>
 	)
 }
+
+
+
+
