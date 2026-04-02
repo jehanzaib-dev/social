ee893880e8cd2d0ffdc743875d61377354e2ce4f
Jehanzaib <jehanzaib.dev@gmail.com>
2026-04-02 12:26:46 +0500

    feed portion updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import HomePage from './pages/home/home.jsx';
-import ProfilePage from './pages/profile/profile.jsx';
-import LoginPage from './pages/login/login.jsx';
-import RegisterPage from './pages/register/register.jsx';
 
 function App() {
   return (
